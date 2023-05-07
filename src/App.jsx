54f2a9813d00a31cb59aabd96863037f820428e7
severu <russcanadilla@gmail.com>
2023-05-07 18:04:44 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import './App.css'
+//import { BeakerIcon } from '@heroicons/react/24/solid'
+
+
+function App() {
+  const [inputs, setInputs] = useState([])
+  const [currentInput, setCurrentInput] = useState('')
+  //const [isChecked, setIsChecked] = useState(false)
+  const [checkedItems, setCheckedItems] = useState([])
+  
+  
+  const inputHandler = (e) => {
+    //e.preventDefault()
+    setCurrentInput(e.target.value)
+  }
+
+  const checkHander = (index) => {
+    
+    //const checkStatus = e.target.checked
+     /*
+    setCheckedItems((prevCheckedItems) => {
+     
+      const updatedCheckedItems = [...prevCheckedItems]      
+      updatedCheckedItems[index] = !updatedCheckedItems[index]    
+      return updatedCheckedItems
+      
+     
+    }) 
+    */
+    const newCheckedItems = [...checkedItems]
+    newCheckedItems[index] = !newCheckedItems[index]
+    setCheckedItems(newCheckedItems)
+    console.log(checkedItems)
+    //setIsChecked(!isChecked)
+  }
+  
+  
+  const clickHandler = (e) => {
+        e.preventDefault()
+      
+        //const newInput = e.target.form[0].value
+        //setCurrentInput(e.target.form[0].value)
+        
+        //setInput([input, currentInput])
+      if(currentInput !== ""){
+        if(inputs.length === 0){
+          setInputs((prevInput) => [...prevInput, currentInput])
+          // console.log('if empty', inputs)
+        }else{
+          setInputs([...inputs, currentInput])
+          // console.log('if not empty', inputs)
+        }
+      }
+        
+        
+        
+        
+        setCurrentInput('')
+        // console.log(currentInput)
+        // console.log(inputs)
+        //setCount(count + 1)
+        //setCount(prevCount => prevCount + 1)
+        //console.log(currentInput)
+  }
+
+  
+
+
+
+  return (
+    <div className='container'>
+        <h1 className='title'>TODO LIST</h1>
+      <div className='form-wrapper'>
+        <form id='form-input'>
+            <label htmlFor="input">
+              <input required className='todo-input' value={currentInput} id='input' onChange={ inputHandler } />
+            </label>
+        </form>
+        <button className="btn" type='submit' form='form-input' onClick={clickHandler}>
+        ADD
+        </button>
+        
+      </div>
+      <div className='todo-list-wrapper'>
+          <div className='todo-list'>
+              <label>{/*To-Do List:*/}</label>
+              <ul>
+                {inputs.map((input, index) => {
+
+                    return ( <li key={index} onClick={e => checkHander(index)} className={checkedItems[index] ? 'strike-tr greyed-out' : ''}>
+                                            <input className='checkbox' type='checkbox' />{input}
+                            
+                            </li>)
+                })}
+              </ul>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
