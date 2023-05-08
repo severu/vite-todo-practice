3ea1bbcead8287be21fa5be6a1f89fd09e869744
severu <russcanadilla@gmail.com>
2023-05-08 16:05:02 +0800

    fixed some css

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ function App() {
               <input required className='todo-input' value={currentInput} id='input' onChange={ inputHandler } />
             </label>
         </form>
-        <button className="btn" type='submit' form='form-input' onClick={clickHandler}> 
+        <button className="add-btn" type='submit' form='form-input' onClick={clickHandler}> 
         {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>*/}
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import './index.css'
+import { BeakerIcon, ArrowDownTrayIcon } from '@heroicons/react/24/solid'
 
 function TodoList({ checkedItems, inputs, checkHandler }) {
   return (
@@ -9,7 +10,11 @@ function TodoList({ checkedItems, inputs, checkHandler }) {
                 {inputs.map((input, index) => {
 
                     return ( <li key={index} onClick={e => checkHandler(index)} className={checkedItems[index] ? 'strike-tr greyed-out' : ''}>
-                                            <input className='checkbox' type='checkbox' />{input}<span></span>
+                                            <input className='checkbox' type='checkbox' />{input}
+                                            <span className='svg-container'>  
+                                                <button className='del-btn'>X
+                                                </button>
+                                            </span>
                             
                             </li>)
                 })}
