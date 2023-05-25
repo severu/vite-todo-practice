5f58eb6b0f7bad73c25647b73c9fc1b0ad58450a
severu <russcanadilla@gmail.com>
2023-05-25 13:28:21 +0800

    added alert using sweetalert2 for delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,111 +1,131 @@
-import { useState } from 'react'
-import './App.css'
-import TodoList from './TodoList'
-import { BeakerIcon, ArrowDownTrayIcon } from '@heroicons/react/24/solid'
+import { useState } from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+import { BeakerIcon, ArrowDownTrayIcon } from "@heroicons/react/24/solid";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
-
+const MySwal = withReactContent(Swal);
 
 function App() {
-  const [inputs, setInputs] = useState([])
-  const [currentInput, setCurrentInput] = useState('')
+  const [inputs, setInputs] = useState([]);
+  const [currentInput, setCurrentInput] = useState("");
   //const [isChecked, setIsChecked] = useState(false)
-  const [checkedItems, setCheckedItems] = useState([])
-  
+  const [checkedItems, setCheckedItems] = useState([]);
+
   const id = crypto.randomUUID();
-  
-  
+
   const inputHandler = (e) => {
     //e.preventDefault()
-    setCurrentInput(e.target.value)
+    setCurrentInput(e.target.value);
   };
 
   const checkHandler = (input) => {
-    console.log(input)
+    console.log(input);
     input.isChecked = !input.isChecked;
-    console.log(input.isChecked)
-    
+    console.log(input.isChecked);
+
     setCheckedItems((prevCheckedItems) =>
-    prevCheckedItems.filter((items) => items.id !== input.id)
+      prevCheckedItems.filter((items) => items.id !== input.id)
     );
-   
   };
 
   const handleDelete = (id) => {
     //const uuid = uuidv4;
     //console.log(id)
-    setInputs((prevInputs) =>
-    prevInputs.filter((inputs) => inputs.id !== id)
-  );
-    
-    setCheckedItems((prevCheckedItems) =>
-      prevCheckedItems.filter((inputs) => inputs.id !== id)
-    );
-    console.log("inside handleDelete",  inputs)
+    //triggers sweetalert before the delete
+    MySwal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      //if user clicks the confirm button, proceed to delete item/list
+      if (result.isConfirmed) {
+        setInputs((prevInputs) =>
+          prevInputs.filter((inputs) => inputs.id !== id)
+        );
+
+        setCheckedItems((prevCheckedItems) =>
+          prevCheckedItems.filter((inputs) => inputs.id !== id)
+        );
+        console.log("inside handleDelete", inputs);
+        Swal.fire("Deleted!", "The item has been deleted.", "success");
+      }
+    });
   };
-  
- 
 
   // event handler for input
   const addClickHandler = (e) => {
-        e.preventDefault()
-        
-        //setInput([input, currentInput])
-      
-      if(currentInput !== ""){
-        if(inputs.length === 0){
-          setInputs((prevInputs) => [...prevInputs, {id: id, todo: currentInput, isChecked: false}])
-          
-          console.log('if empty', inputs)
-        }else{
-          setInputs([...inputs, {id: id, todo: currentInput, isChecked: false}])
-         
-          console.log('not empty', inputs)
-          
-        }
-      } 
- 
-      setCurrentInput('')
-      console.log(id)       
+    e.preventDefault();
+
+    //setInput([input, currentInput])
+
+    if (currentInput !== "") {
+      if (inputs.length === 0) {
+        setInputs((prevInputs) => [
+          ...prevInputs,
+          { id: id, todo: currentInput, isChecked: false },
+        ]);
+
+        console.log("if empty", inputs);
+      } else {
+        setInputs([
+          ...inputs,
+          { id: id, todo: currentInput, isChecked: false },
+        ]);
+
+        console.log("not empty", inputs);
+      }
+    }
+
+    setCurrentInput("");
+    console.log(id);
   };
 
-  
-  
-
-
-
   return (
-    <div className='container'>
-        <h1 className='title'>TODO LIST</h1>
-      <div className='form-wrapper'>
-        <form id='form-input'>
-            <label htmlFor="input">
-              <input required className='todo-input' value={currentInput} id='input' onChange={ inputHandler } />
-            </label>
+    <div className="container">
+      <h1 className="title">TODO LIST</h1>
+      <div className="form-wrapper">
+        <form id="form-input">
+          <label htmlFor="input">
+            <input
+              required
+              className="todo-input"
+              value={currentInput}
+              id="input"
+              onChange={inputHandler}
+            />
+          </label>
         </form>
-        <button className="add-btn" type='submit' form='form-input' onClick={addClickHandler}> 
-        {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+        <button
+          className="add-btn"
+          type="submit"
+          form="form-input"
+          onClick={addClickHandler}
+        >
+          {/*<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>*/}
-        
-        <ArrowDownTrayIcon className="h-6 w-6 text-blue-500" />
+
+          <ArrowDownTrayIcon className="h-6 w-6 text-blue-500" />
         </button>
-        
       </div>
-      <div className='todo-list-wrapper'>
-          <div className='todo-list'>
-              <label>{/*To-Do List:*/}</label>
-              <TodoList 
-                        inputs={inputs}
-                        checkHandler={checkHandler} 
-                        handleDelete={handleDelete}
-                        
-              />
-          </div>
+      <div className="todo-list-wrapper">
+        <div className="todo-list">
+          <label>{/*To-Do List:*/}</label>
+          <TodoList
+            inputs={inputs}
+            checkHandler={checkHandler}
+            handleDelete={handleDelete}
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,70 +1,68 @@
-import React from 'react'
-import './App.css'
-import './index.css'
-import { useState } from 'react'
+import React from "react";
+import "./App.css";
+import "./index.css";
+
+import { useState } from "react";
 import { Popover } from "react-tiny-popover";
-import { BeakerIcon, ArrowDownTrayIcon } from '@heroicons/react/24/solid'
-
-
-
+import { BeakerIcon, ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 
 function TodoList({ inputs, checkHandler, handleDelete, id }) {
-
-  
-
-  
-
-  
   return (
     <>
-        <ul>
-                {inputs.toReversed().map((input, index) => {
-                      //console.log('uuid', id)
-                      return ( <li key={input.id} onClick={e => {
-                        
-                        const div = e.currentTarget;
-                        const edit = div.querySelector('.edt-btn');
-                        const del = div.querySelector('.del-btn');
-                        const close = div.querySelector('.close-btn');
-                        //console.log(edit)
-                        if(edit === e.target || del === e.target || close === e.target){
-                          
-                          return; 
-                        }else{
-                          
-                          return checkHandler(input);
-                        }
-                        
-                  }
-                              
-                              } className={input.isChecked ? 'strike-tr' : ''}>
-                                      <div className='chk-box-container'>
-                                          <input hidden className='checkbox' type='checkbox' />
-                                          <span>{input.todo}</span>
-                                      </div>
-                                          
-                                          
-                                          <span className='svg-del-container'>  
-                                              <button className='edt-btn' 
-                                                      onClick={() => {handleEdit(input.id)}}>EDIT
-                                              </button>   
-                                              
-                                              <button className='del-btn' 
-                                                      onClick={() => {handleDelete(input.id)}}>X
-                                              </button>      
-                                          </span>
-                                          
-                                                    
-                                              
-                          
-                          </li>)
-                    
+      <ul>
+        {inputs.toReversed().map((input, index) => {
+          //console.log('uuid', id)
+          return (
+            <li
+              key={input.id}
+              onClick={(e) => {
+                const div = e.currentTarget;
+                const edit = div.querySelector(".edt-btn");
+                const del = div.querySelector(".del-btn");
+                const close = div.querySelector(".close-btn");
+                //console.log(edit)
+                if (
+                  edit === e.target ||
+                  del === e.target ||
+                  close === e.target
+                ) {
+                  return;
+                } else {
+                  return checkHandler(input);
+                }
+              }}
+              className={input.isChecked ? "strike-tr" : ""}
+            >
+              <div className="chk-box-container">
+                <input hidden className="checkbox" type="checkbox" />
+                <span>{input.todo}</span>
+              </div>
 
-                    
-                })}
-        </ul>
+              <span className="svg-del-container">
+                <button
+                  className="edt-btn"
+                  onClick={() => {
+                    handleEdit(input.id);
+                  }}
+                >
+                  EDIT
+                </button>
+
+                <button
+                  className="del-btn"
+                  onClick={() => {
+                    handleDelete(input.id);
+                  }}
+                >
+                  X
+                </button>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
