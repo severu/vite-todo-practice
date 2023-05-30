8b786d20e0f493ec8da6ec735ae86120cc4273fb
severu <russcanadilla@gmail.com>
2023-05-30 18:29:20 +0800

    changed theme basic button to toggle button

--- src/components/ButtonToggler/ButtonToggler.jsx
+++ src/components/ButtonToggler/ButtonToggler.jsx
@@ -4,8 +4,16 @@ import "./ButtonToggler.modules.scss";
 function ButtonToggler({ isDarkMode, toggleTheme }) {
   return (
     <div className="button-container">
-      
-      <button className="button-container__button" onClick={() => toggleTheme()}>Toggle</button>
+      <label className="button-container__switch">
+        <input
+          className="button-container__switch__checkbox"
+          type="checkbox"
+          onChange={() => toggleTheme()}
+        />
+        <span className="button-container__switch__checkbox__slider-round"></span>
+      </label>
+
+      {/*<button className="button-container__button" onClick={() => toggleTheme()}>Toggle</button>*/}
     </div>
   );
 }
